6f9b971e1197d5ca06bce02013b5f9f0d4134a79
angp-mtz <123033185+angp-mtz@users.noreply.github.com>
2023-10-08 03:53:06 -0500

    module 1

---
+++ labs/average.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+round1 = int(input("enter score for round 1: "))
+round2 = int(input("enter score for round 2: "))
+round3 = int(input("enter score for round 3: "))
+average = (round1 + round2 + round3) / 3
+print ("the average score is: ", average)
